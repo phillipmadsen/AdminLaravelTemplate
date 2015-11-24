0638c07a5909932c2e2bf629edf87fe7007b9040
Phillip Madsen <pmadsen2013@gmail.com>
2015-11-24 12:21:36 -0700

    removed old structure

--- aprogrammer/admintemplatelaravel/src/resources/views/partials/jsheader.blade.php
+++
@@ -1,9 +0,0 @@
-    <!--[if lt IE 9]>
-    <script src="{!! asset('/assets/plugins/respond.min.js') !!}"></script>
-    <script src="{!! asset('/assets/plugins/excanvas.min.js') !!}"></script>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <![endif]-->
-    <!--[if gte IE 9]><!-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-    <!--<![endif]-->
-    <script src="{!! asset('/assets/plugins/jquery-ui/jquery-ui-1.10.2.custom.min.js') !!}"></script>
--- aprogrammer/admintemplatelaravel/src/resources/views/partials/pageheader.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- start: PAGE HEADER -->
-<div class="row">
-  <div class="col-sm-12">
-    @include('partials.styleselector')
-    @yield('pagetitleblock')
-    {{-- @include('partials.pagetitleblock') --}}
-  </div>
-</div>
-<!-- end: PAGE HEADER -->
